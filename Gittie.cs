1d4524355800146615861bbe4d1757f4efa9eee2
a_michalak <mot+eaglethemighty@users.noreply.github.com>
2021-08-17 10:09:48 +0200

    Implement joke function

--- Gittie.cs
+++ Gittie.cs
@@ -3,3 +3,9 @@ public void introduce()
 {
     Console.Writeline("Hellom I'm Gittie");
 }
+
+public void joke()
+{
+    Console.Writeline("What's the best thing about Switzerland?");
+    Console.Writeline("I don't know, but the flag is a big plus");
+}
